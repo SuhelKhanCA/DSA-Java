2f78711afcccdcfae936df237082f3fcbda0b85f
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-02-04 19:24:04 +0000

    DSA

--- KK/Shadow.java
+++
@@ -1,9 +0,0 @@
-public class Shadow {
-   static int x=50; // Shadowed at line 
-    public static void main(String[] args) {
-        System.out.println(x);
-
-        int x=100;
-        System.out.println(x);
-    }
-}
