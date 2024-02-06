e88109eddb9214401edf576de7393300b6e28482
suhelkhanca <suhel.hata@gmail.com>
2024-02-06 13:59:37 +0530

    Create a vector of default size

---
+++ Six6MonthsOfCode/Day14/VectorDemo.java
@@ -0,0 +1,15 @@
+package Six6MonthsOfCode.Day14;
+
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector v = new Vector(); // Create a vector of default size : 10
+        v.add(1);
+        v.add(2);
+        v.add("Debasis");
+        v.add(3.2);
+        v.add("Samanta");
+        System.out.println("Vector is : " + v);
+    }
+}
