73c8b4dc832f2dba5a711bd092c6afaee6ea4fc5
suhelkhanca <suhel.hata@gmail.com>
2024-02-06 13:54:53 +0530

    traversing a sublist of an array

---
+++ Six6MonthsOfCode/Day13/TraverseSublist.java
@@ -0,0 +1,12 @@
+package Six6MonthsOfCode.Day13;
+
+import java.util.Arrays;
+
+public class TraverseSublist { // traversing a sublist of an array
+    public static void main(String[] args) {
+        // Get the array
+        int arr[] = {10, 20, 15, 22, 35};
+        // to sort the array using normal sort
+        System.out.println("Integer Array : " + Arrays.spliterator(arr, 1, 3));
+    }
+}
