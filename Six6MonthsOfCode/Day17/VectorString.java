112e0eca5782fd5cf83139c91587848b14a16a36
suhelkhanca <suhel.hata@gmail.com>
2024-02-06 15:40:35 +0530

    string equivalent of a vector

---
+++ Six6MonthsOfCode/Day17/VectorString.java
@@ -0,0 +1,21 @@
+package Six6MonthsOfCode.Day17;
+
+import java.util.Arrays;
+import java.util.Vector;
+
+public class VectorString {
+    public static void main(String[] args) {
+         Vector vec = new Vector<>(); //
+
+        vec.add(1);
+        vec.add(2);
+        vec.add(3);
+        vec.add(4);
+        vec.add(5);
+        vec.add(6);
+        vec.add(7);
+
+        // string equivalent of a vector
+        System.out.println("String equivalent of a vector: " + vec.toString());
+    }
+}
