730ea3945f9bf6a3774846b7ff945e722d3f6649
suhelkhanca <suhel.hata@gmail.com>
2024-02-07 16:40:50 +0530

    Generic method to reverse the arrays of different types

--- Generics/Lec3/GenericArray.java
+++ Generics/Lec3/GenericArray.java
@@ -5,57 +5,66 @@ package Generics.Lec3;
 class GenericArray<T> {
 
     // Declaring an array, which should store any type T of data
-
-    T a[];
+    T[] a;
 
     GenericArray(T[] x) { // Define a constructor
-
         a = x;
-
     }
 
     T getData(int i) { // To return the element stored in the i-th place in the array
-
         return a[i];
-
     }
 
-    public void printData(T b) {// A generic method to print the elements in array b
-
-        for (int i = 0; i < b.length; i++)
-
-            System.out.print(b.getData(i) + " "); // Print the i-th element in b
-
+    public void printData() { // A method to print the elements in array a
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " "); // Print the i-th element in a
         System.out.println(); // Print a new line
-
     }
 
-    // Generic method to reversed the order of elements in array b
-
-     void reverseArray(T b) {
-
-        int front = 0, rear = b.length - 1;
-
+    // Method to reverse the order of elements in array a
+    void reverseArray() {
+        int front = 0, rear = a.length - 1;
         T temp;
-
         while (front < rear) {
-
-            temp = b[rear];
-
-            b[rear] = b[front];
-
-            b[front] = temp;
-
+            temp = a[rear];
+            a[rear] = a[front];
+            a[front] = temp;
             front++;
             rear--;
-
         }
-
     }
 
     // main method
     public static void main(String[] args) {
-       // code for main method
+        // Create a GenericArray for Integers
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        GenericArray<Integer> gaInt = new GenericArray<>(intArray);
+    
+        // Print the original array
+        System.out.println("Original array:");
+        gaInt.printData();
+    
+        // Reverse the array
+        gaInt.reverseArray();
+    
+        // Print the reversed array
+        System.out.println("Reversed array:");
+        gaInt.printData();
+    
+        // Create a GenericArray for Strings
+        String[] strArray = {"Java", "Python", "C++", "JavaScript", "Go"};
+        GenericArray<String> gaStr = new GenericArray<>(strArray);
+    
+        // Print the original array
+        System.out.println("Original array:");
+        gaStr.printData();
+    
+        // Reverse the array
+        gaStr.reverseArray();
+    
+        // Print the reversed array
+        System.out.println("Reversed array:");
+        gaStr.printData();
     }
-
+    
 }
