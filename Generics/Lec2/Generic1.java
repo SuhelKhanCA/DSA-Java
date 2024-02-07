26877c1698cb1127f02ef5f66f3fcb20fa40b735
suhelkhanca <suhel.hata@gmail.com>
2024-02-07 14:50:30 +0530

    Defining a generic method to print any data type

---
+++ Generics/Lec2/Generic1.java
@@ -0,0 +1,26 @@
+// Example 2.1: A generic method for printing
+package Generics.Lec2;
+
+class DemoClass<T> { // <T>
+
+    // Defining a generic method to print any data type
+
+    void genericPrint(T t) {
+
+        System.out.println(t);
+
+    }
+
+    public static void main(String[] args) {
+
+        DemoClass aObj = new DemoClass(); // Creating an object of the class DemoClass
+
+        aObj.genericPrint(101); // Calling generic method with int argument
+
+        aObj.genericPrint("Joy with Java"); // Calling generic method with String
+
+        aObj.genericPrint(3.1412343); // Calling generic method with double
+
+    }
+
+}
