0f22148a86e860ecb58666c89c2a9a87598de8f9
suhelkhanca <suhel.hata@gmail.com>
2024-02-10 22:00:41 +0530

    a simple singly linked-list

---
+++ LinkedList/JLinkedList.java
@@ -0,0 +1,21 @@
+// This program show how to define a singly linked-list
+class JLinkedList<T>{
+    Node head; // head of list
+    class Node{
+        T data;
+        Node next;
+        // constructor
+        Node(){
+            data = null;
+            next = null;
+        }
+        Node(T d){
+            data = d;
+            next = null;
+        }
+    }
+    JLinkedList(){ // Linked-list HEADER node
+        head = new Node();
+    }
+    // Methods to manipulate linked-list to be defined
+}
