b4739a3bc22a6516f81753819e1cfb80fef6eeb4
suhelkhanca <suhel.hata@gmail.com>
2024-02-10 22:32:48 +0530

    insertion into linked-list

---
+++ LinkedList/InsertionAtFront.java
@@ -0,0 +1,4 @@
+public class InsertionAtFront<T> {
+    /* Defining the method to insert at the front of list */
+    
+}
--- LinkedList/JLinkedList.java
+++ LinkedList/JLinkedList.java
@@ -1,5 +1,5 @@
 // This program show how to define a singly linked-list
-class JLinkedList<T>{ // EXAMPLE 18.1
+public class JLinkedList<T>{ // EXAMPLE 18.1
     Node head; // head of list
     class Node{
         T data;
@@ -18,4 +18,69 @@ class JLinkedList<T>{ // EXAMPLE 18.1
         head = new Node();
     }
     // Methods to manipulate linked-list to be defined
+    /* Defining the method to insert at the front of list */
+    public void insertFront(T data){
+        // create a new node with given data
+        Node newNode = new Node(data);
+        newNode.next = this.head.next;
+        // make the new node as the first node
+        this.head.next = newNode;
+    }
+
+    /* INSERTION AT END */
+    public void insertionEnd(T data){
+        Node newNode = new Node(data);
+        newNode.next = null;
+        Node temp = this.head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    /* INSERTION AT ANY POSITION */
+    public void insertKey(T data, T key){
+        Node newNode = new Node(data);
+        newNode.next = null;
+        Node temp = this.head;
+        boolean status = false;
+        while (temp != null) {
+            if (temp.data == key) {
+                status = true;
+                break;
+            }
+            temp = temp.next;
+            
+        }
+        if (status) {
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+    }
+
+    /* PRINTING THE LINKED-LIST */
+    public void printList(){
+        Node curNode = this.head.next;
+        System.out.print("LinkedList: ");
+        // traverse through linked-list
+        while (curNode != null) {
+            // print the data at current node
+            System.out.print(curNode.data + " ");
+            // got to next node
+            curNode = curNode.next;
+        }
+        System.out.println();
+    }
+    class LinkedListInsertionDemo{
+        public static void main(String[] args) {
+            JLinkedList<Integer> list = new JLinkedList<Integer>();
+            list.insertionEnd(9);
+            list.printList();
+            list.insertFront(5);
+            list.printList();
+            list.insertKey(7, 5);
+            list.printList();
+        }
+    }
 }
