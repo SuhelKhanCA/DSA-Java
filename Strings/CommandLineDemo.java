b2d4c56da3116eba0050949fc33f957a6768b11f
suhelkhanca <suhel.hata@gmail.com>
2024-04-28 15:15:44 +0530

    String as command line arguments

---
+++ Strings/CommandLineDemo.java
@@ -0,0 +1,19 @@
+// Example 56.2: String as command line arguments
+
+class CommandLineDemo {
+
+    public static void main(String args[]) {
+
+        System.out.println("Number of arguments" + " " + args.length);
+
+        if (args.length == 0)
+
+            return;
+
+        for (int i = 0; i < args.length; i++)
+
+            System.out.println("args[" + i + "]: " + args[i]);
+
+    }
+
+}
