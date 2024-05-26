0d2ac764bc586ca6406a131d587d33cad71fb2ba
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2024-05-26 16:37:47 +0000

    replace() function in Java

---
+++ Strings/ReplaceString.java
@@ -0,0 +1,7 @@
+class ReplaceString{
+    public static void main(String[] args){
+        String text = "Data Structure with C++";
+        String output = text.replace("C++", "Java");
+        System.out.println(output);
+    }
+}
