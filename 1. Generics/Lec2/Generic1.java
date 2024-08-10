28a6aa7d34ed5ae22ab51c6f838c270cad4099f4
suhelkhanca <suhel.hata@gmail.com>
2024-08-10 19:08:56 +0530

    Printing various data types using single generic method

--- 1. Generics/Lec2/Generic1.java
+++ 1. Generics/Lec2/Generic1.java
@@ -1,17 +1,26 @@
+// Example 2.1: A generic method for printing
 package Generics.Lec2;
 
-class DemoClass<T> {
+class DemoClass<T> { // <T>
 
     // Defining a generic method to print any data type
-    void genericPrint(T t) {
+
+   static <T> void genericPrint(T t) {
+
         System.out.println(t);
+
     }
 
     public static void main(String[] args) {
-        DemoClass aObj = new DemoClass();
 
-        aObj.genericPrint(Integer.valueOf(101)); // Using Integer wrapper
-        aObj.genericPrint("Joy with Java");
-        aObj.genericPrint(Double.valueOf(3.1412343)); // Using Double wrapper
+        DemoClass aObj = new DemoClass(); // Creating an object of the class DemoClass
+
+        aObj.genericPrint(101); // Calling generic method with int argument
+
+        aObj.genericPrint("Joy with Java"); // Calling generic method with String
+
+        aObj.genericPrint(3.1412343); // Calling generic method with double
+
     }
+
 }
