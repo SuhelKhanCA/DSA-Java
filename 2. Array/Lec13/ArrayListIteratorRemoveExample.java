b0cc83db437cdeb0f4be1b7ee84bb718c9374749
suhelkhanca <suhel.hata@gmail.com>
2024-08-11 12:06:43 +0530

    Traversing and removing elements from an ArrayList collection

---
+++ 2. Array/Lec13/ArrayListIteratorRemoveExample.java
@@ -0,0 +1,24 @@
+// Example 13.12: Traversing and removing elements from an ArrayList collection
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrayListIteratorRemoveExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(13);
+        numbers.add(18);
+        numbers.add(25);
+        numbers.add(40);
+
+        Iterator<Integer> numbersIterator = numbers.iterator();
+        while (numbersIterator.hasNext()) {
+            Integer num = numbersIterator.next();
+            if (num % 2 != 0) {
+                numbersIterator.remove();
+            }
+        }
+        System.out.println(numbers);
+    }
+}
