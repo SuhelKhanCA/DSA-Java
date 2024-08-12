da2e7a9106c59b23e7e7d02364af64be8b941857
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:24:28 +0530

    eqauls() method

---
+++ 6. Strings/Lec57/Example57_1.java
@@ -0,0 +1,15 @@
+// equals() method
+class StringEqual{
+    public static void main(String[] args){
+        String text1 = "DATA STRUCTURE WITH JAVA";
+        String text2 = "DATA STRUCTURE WITH C++";
+        String text3 = "DATA STRUCTURE WITH JAVA";
+
+        boolean opt1 = text1.equals(text2);
+        boolean opt2 = text1.equals(text3);
+
+        System.out.println(opt1);
+        System.out.println(opt2);
+        
+    }
+}
