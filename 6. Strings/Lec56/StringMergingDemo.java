d43a0a4bda7ef55c9b09dbc75b3732c50369b941
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:14:11 +0530

    concat()

---
+++ 6. Strings/Lec56/StringMergingDemo.java
@@ -0,0 +1,9 @@
+// Example 56.7: concat()
+public class StringMergingDemo {
+    public static void main(String args[]) {
+        String text1 = "DATA STRUCTURE WITH";
+        String text2 = " JAVA";
+        String text3 = text1.concat(text2);
+        System.out.print(text3);
+    }
+}
