555fbcede1bee1230afff77c6b5b4c75cb8382f0
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:15:02 +0530

    toLowerCase()

---
+++ 6. Strings/Lec56/StringToLowerCaseDemo.java
@@ -0,0 +1,8 @@
+// Example 56.8: toLowerCase()
+public class StringToLowerCaseDemo {
+    public static void main(String args[]) {
+        String text = "Data Structure with Java";
+        String output = text.toLowerCase();
+        System.out.println(output);
+    }
+}
