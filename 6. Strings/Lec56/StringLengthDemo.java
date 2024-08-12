a09372baa761d81fd5961a6f0ab8d43ce65624e7
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:12:17 +0530

    length() of String

---
+++ 6. Strings/Lec56/StringLengthDemo.java
@@ -0,0 +1,8 @@
+// Example 56.5: length() of String
+public class StringLengthDemo {
+    public static void main(String args[]) {
+        String text = "DATA STRUCTURE WITH JAVA";
+        int stringLength = text.length();
+        System.out.print(stringLength);
+    }
+}
