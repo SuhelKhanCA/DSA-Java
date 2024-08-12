5f136189ee68ef345e7c40fb0d6209df1a100d69
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:10:43 +0530

    Create a String using literal and print

---
+++ 6. Strings/Lec56/CreateStringDemo.java
@@ -0,0 +1,7 @@
+// Example 56.4: Create a String using literal and print
+public class CreateStringDemo {
+    public static void main(String args[]) {
+        String text = "DATA STRUCTURE WITH JAVA";
+        System.out.print(text);
+    }
+}
