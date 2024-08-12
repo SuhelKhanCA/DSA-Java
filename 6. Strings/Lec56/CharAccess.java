18af43fd093b504f47bbe98182170d3b0c7d4a17
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:13:22 +0530

    charAt()

---
+++ 6. Strings/Lec56/CharAccess.java
@@ -0,0 +1,8 @@
+// Example 56.6: charAt()
+public class CharAccess {
+    public static void main(String args[]) {
+        String text = "DATA STRUCTURE WITH JAVA";
+        char data = text.charAt(5);
+        System.out.print(data);
+    }
+}
