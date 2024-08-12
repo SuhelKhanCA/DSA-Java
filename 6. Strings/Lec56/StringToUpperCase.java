47aa268187b4c1b7557393465c2fe491aa282901
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:16:24 +0530

    toUpperCase() demo

---
+++ 6. Strings/Lec56/StringToUpperCase.java
@@ -0,0 +1,8 @@
+// Example 56.9: toUpperCase()
+public class StringToUpperCaseDemo {
+    public static void main(String args[]) {
+        String text = "Data Structure with Java";
+        String output = text.toUpperCase();
+        System.out.println(output);
+    }
+}
