799694fe356f6e12d42e58f9eb6c5bbff9b8c5fb
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:18:48 +0530

    replace() method

---
+++ 6. Strings/Lec56/StringReplaceDemo.java
@@ -0,0 +1,8 @@
+// Example 56.11: replace()
+public class StringReplaceDemo {
+    public static void main(String args[]) {
+        String text = "Data Structure with C++";
+        String output = text.replace("C++", "Java");
+        System.out.println(output);
+    }
+}
