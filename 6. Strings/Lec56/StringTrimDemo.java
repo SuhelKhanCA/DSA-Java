027de74984e229c8c52a982c7b46e7e896562e35
suhelkhanca <suhel.hata@gmail.com>
2024-08-12 10:17:57 +0530

    trim() - remove heading and trailing white spaces from string

---
+++ 6. Strings/Lec56/StringTrimDemo.java
@@ -0,0 +1,8 @@
+// Example 56.10: trim()
+public class StringTrimDemo {
+    public static void main(String args[]) {
+        String text = "   Data Structure with Java   ";
+        String output = text.trim();
+        System.out.println(output);
+    }
+}
