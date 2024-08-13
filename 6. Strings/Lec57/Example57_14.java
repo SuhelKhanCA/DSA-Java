3006341ccd308889bedd6245ee568322b86439aa
suhelkhanca <suhel.hata@gmail.com>
2024-08-13 19:39:46 +0530

    float to string

---
+++ 6. Strings/Lec57/Example57_14.java
@@ -0,0 +1,8 @@
+// Example 57.14: Float to String
+class FloatToStringDemo {
+    public static void main(String[] args) {
+        float f = 15.5F; // F is the suffix for float
+        String s = String.valueOf(f);
+        System.out.println(s);
+    }
+}
