e5983f6218e9d79918c580f7bf25a608c9f08cb3
suhelkhanca <suhel.hata@gmail.com>
2024-08-13 19:35:03 +0530

    String to Integer

---
+++ 6. Strings/Lec57/Example57_10.java
@@ -0,0 +1,9 @@
+// Example 57.10: String to Integer
+class StringToIntegerDemo {
+    public static void main(String[] args) {
+        String number = "95";
+        int num = Integer.parseInt(number);
+        int output = num + 5;
+        System.out.println(output);
+    }
+}
