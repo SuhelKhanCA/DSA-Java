77dac943f858d76776614786ad6b6e691d1d7032
suhelkhanca <suhel.hata@gmail.com>
2024-08-13 19:29:45 +0530

    String substring(int i) Returns the substring starting from the character with i-th index

---
+++ 6. Strings/Lec57/Example57_5.java
@@ -0,0 +1,9 @@
+// Example 57.5: substring()
+// String substring(int i) Returns the substring starting from the character with i-th index.
+class SubstringDemo {
+    public static void main(String[] args) {
+        String text = "DATA STRUCTURE WITH JAVA";
+        String data = text.substring(3);
+        System.out.print(data);
+    }
+}
