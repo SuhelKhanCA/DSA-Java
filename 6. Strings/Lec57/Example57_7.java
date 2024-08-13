0135408296f4825d719fb1f36ba77fb46740a86c
suhelkhanca <suhel.hata@gmail.com>
2024-08-13 19:32:37 +0530

    int indexOf(String s) Returns the index within the string of the first occurrence of the specified string

---
+++ 6. Strings/Lec57/Example57_7.java
@@ -0,0 +1,9 @@
+// Example 57.7: indexOf() method
+// int indexOf(String s) Returns the index within the string of the first occurrence of the specified string.
+class StringSearchDemo {
+    public static void main(String[] args) {
+        String text = "DATA STRUCTURE WITH JAVA";
+        int output = text.indexOf("WITH");
+        System.out.print(output);
+    }
+}
