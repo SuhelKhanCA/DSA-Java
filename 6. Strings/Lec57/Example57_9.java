b32a7eaa39acdedee3077a6f694fed8953310db3
suhelkhanca <suhel.hata@gmail.com>
2024-08-13 19:34:15 +0530

    int lastIndexOf(String s) Returns the index within the string of the last occurrence of the specified string

---
+++ 6. Strings/Lec57/Example57_9.java
@@ -0,0 +1,9 @@
+// Example 57.9: lastIndexOf()
+// int lastIndexOf(String s) Returns the index within the string of the last occurrence of the specified string.
+class StringLastIndexOfExample {
+    public static void main(String[] args) {
+        String text = "DATA STRUCTURE WITH JAVA";
+        int output = text.lastIndexOf("T");
+        System.out.print(output);
+    }
+}
