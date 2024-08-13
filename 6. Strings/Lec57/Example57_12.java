27a7db03124c3463d0756271d9e8fa043ae5c4ab
suhelkhanca <suhel.hata@gmail.com>
2024-08-13 19:36:24 +0530

    string to double

---
+++ 6. Strings/Lec57/Example57_12.java
@@ -0,0 +1,9 @@
+// Example 57.12: String to Double
+public class StringToDoubleDemo {
+    public static void main(String[] args) {
+        String number = "95.59";
+        double num = Double.parseDouble(number);
+        double output = num + 4.41;
+        System.out.println(output);
+    }
+}
