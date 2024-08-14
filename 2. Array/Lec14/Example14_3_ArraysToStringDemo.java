090a3ed53b6717367b9132c0c155c671bbae6bba
suhelkhanca <suhel.hata@gmail.com>
2024-08-14 20:20:48 +0530

    Print the array as a string

---
+++ 2. Array/Lec14/Example14_3_ArraysToStringDemo.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Example14_3_ArraysToStringDemo {
+    public static void main(String[] args) {
+        int[] intArr = {10, 20, 15, 22, 35}; // Get the array
+        // Print the array as a string
+        System.out.println("Integer Array: " + Arrays.toString(intArr));
+    }
+}
