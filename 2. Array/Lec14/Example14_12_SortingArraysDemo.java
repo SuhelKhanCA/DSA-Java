431d65aa2e18b8ba37be2116ea21c886d1e94df9
suhelkhanca <suhel.hata@gmail.com>
2024-08-14 20:34:24 +0530

    Sort the array

---
+++ 2. Array/Lec14/Example14_12_SortingArraysDemo.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Example14_12_SortingArraysDemo {
+    public static void main(String[] args) {
+        int[] intArr = {10, 20, 15, 22, 35}; // Get the array
+        Arrays.sort(intArr); // Sort the array
+        System.out.println("Sorted Integer Array: " + Arrays.toString(intArr));
+    }
+}
