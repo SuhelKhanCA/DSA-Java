341fa5b6a5ca92dc9931be563bf63f6bb235ac35
suhelkhanca <suhel.hata@gmail.com>
2024-08-14 20:17:19 +0530

    converting Array as List

---
+++ 2. Array/Lec14/Example14_1_ArrayToListDemo.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Example14_1_ArrayToListDemo {
+    public static void main(String[] args) {
+        int[] intArr = {10, 20, 15, 22, 35}; // An array of int
+        // Convert the array to a list
+        System.out.println("Integer Array as List: " + Arrays.asList(intArr));
+    }
+}
