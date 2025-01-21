0f48a37931a64c0153a1c9b3fa3f2f26e5f47e17
suhelkhanca <suhel.hata@gmail.com>
2025-01-21 20:41:08 +0530

    creating all three iterators on Vector collection

---
+++ Cursors/Example606.java
@@ -0,0 +1,18 @@
+import java.util.Enumeration;
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class Example606 {
+    public static void main(String[] args) {
+        // create a Vector
+        Vector<Integer> v = new Vector<>();
+
+        // create three iterators
+        Enumeration<Integer> e = v.elements();
+        Iterator<Integer> itr = v.iterator();
+        ListIterator<Integer> ltr = v.listIterator();
+
+        // USE
+        
+    }
+}
